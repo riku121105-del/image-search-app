964621e149f5bd356f2e7d56a7318f028f43f550
riku121105-del <riku121105@gmail.com>
2026-09-23 01:08:31 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Googleレンズ風 Web画像検索AI", layout="wi
 st.title("🔍 Googleレンズ風 Web類似画像検索AI")
 
 # --- SerpApi 設定 ---
-SERPAPI_KEY = "d40d84efb3725876af1c33b63baf4..."  # ご自身のAPIキー
+SERPAPI_KEY = "d40d84efb3725876af1c33b63baf4..."
 
 
 def get_public_image_url(image_bytes):
